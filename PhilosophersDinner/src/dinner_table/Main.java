6c4860d6d734d9f613189e5a8c61f27ebaada336
HCmenezes <hectorfrancamenezes@gmail.com>
2025-09-25 19:33:01 -0300

    Primeiro commit

---
+++ PhilosophersDinner/src/dinner_table/Main.java
@@ -0,0 +1,16 @@
+package dinner_table;
+
+/* 
+5 filósofos, 5 garfos, um filósofo precisa de dois garfos para comer
+O filósofo alterna entre COMER e PENSAR. 
+Quando um filósofo fica faminto, ele tenta pegar seus garfos um de cada vez, não importa a ordem
+Se pegar, ele come por um tempo, então para e continua a pensar
+*/
+public class Main {
+	static int[] forks = new int[4];
+	
+	public static void main() {
+		System.out.println(forks);
+	}
+	
+}
